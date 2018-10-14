1144ae3c127f5855f05f359d85ad6f569d879fcb
Konstantin M <konstantindev925@gmail.com>
2018-10-14 11:25:53 -0700

    fix bottom spacing on contact form

--- pages/contact.js
+++ pages/contact.js
@@ -19,7 +19,7 @@ export default class Contact extends React.Component {
         <div className="animated fadeIn">
           <Wrapper className="animated fadeInUp" style={{ animationDelay: '400ms' }}>
             <div className="row">
-              <div className="col-md-12 col-lg-8 mx-auto">
+              <div className="col-md-12 col-lg-8 mx-auto" style={{ marginBottom: '3rem' }}>
                 {DISPLAY_CONTACT_FORM && <ContactForm />}
                 {!DISPLAY_CONTACT_FORM && <ContactStatic />}
               </div>
