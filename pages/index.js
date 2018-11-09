967a50ab41992c592dc032dccf25a633e40e0b33
Konstantin M <konstantindev925@gmail.com>
2018-11-09 08:25:34 -0800

    form/ui improvements

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { HomePage, Hero, HeroColumns, HeroCallout, Testimonials } from '../components';
 
 export default () => (
