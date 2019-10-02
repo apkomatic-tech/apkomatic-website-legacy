d5beb243192bc582594b5e89b38d2f81deb27549
Konstantin M <humbl3dev@gmail.com>
2019-10-01 23:09:43 -0700

    wip

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,30 +4,32 @@ import Link from "next/link"
 export default () => (
   <>
     <div className="hero mb-0">
-      <div
-        className="container animated fadeIn"
-        style={{ animationDuration: "400ms" }}
-      >
-        <img
-          className="img-fluid hero__image"
-          src="/static/apkomatic-hero.svg"
-          alt="hero"
-        />
-        <h1 className="hero__heading">
-          Let's take your website <br /> to the next level.
+      <div className="container">
+        <h1
+          className="hero__heading animated fadeInUp"
+          style={{
+            animationDelay: "240ms"
+          }}
+        >
+          Take your website <br /> to the next level.
         </h1>
-        <p className="hero__paragraph">
-          At Apkomatic we are dedicated to deliver high quality website
-          solutions.
-        </p>
+        <h2
+          className="hero__subheading animated fadeInUp"
+          style={{
+            animationDelay: "500ms"
+          }}
+        >
+          Apkomatic is dedicated to deliver high quality website solutions.
+        </h2>
         <Link href="/contact">
           <a
-            className="hero__cta btn btn-lg btn-tertiary"
+            className="hero__cta btn btn-lg btn-primary animated fadeInUp"
             style={{
-              fontSize: "2rem"
+              fontSize: "2rem",
+              animationDelay: "1000ms"
             }}
           >
-            Get a Quote
+            Let's Start Building
           </a>
         </Link>
       </div>
