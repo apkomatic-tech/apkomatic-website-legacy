5415e53f769ca45a3928cdf8c1849539286c78df
Konstantin M <humbl3dev@gmail.com>
2019-10-09 13:29:58 -0700

    dev

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -39,34 +39,6 @@ export default () => (
             }}
           />
         </div>
-
-        {/* <h1
-          className="hero__heading animated fadeInUp"
-          style={{
-            animationDelay: "240ms"
-          }}
-        >
-          Take your website <br /> to the next level.
-        </h1>
-        <h2
-          className="hero__subheading animated fadeInUp"
-          style={{
-            animationDelay: "500ms"
-          }}
-        >
-          Apkomatic is dedicated to deliver high quality website solutions.
-        </h2>
-        <Link href="/contact">
-          <a
-            className="hero__cta btn btn-lg btn-primary animated fadeInUp"
-            style={{
-              fontSize: "2rem",
-              animationDelay: "1000ms"
-            }}
-          >
-            Let's Start Building
-          </a>
-        </Link> */}
       </div>
     </div>
   </>
