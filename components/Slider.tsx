c7cc17b5d288d70212f3b794a00226fb541ee019
Konstantin M <humbl3dev@gmail.com>
2019-10-12 16:14:02 -0700

    removes slick slider component and references to dead code

--- components/Slider.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SlickSlider from 'react-slick';
-
-const Slider = ({ settings, children }) => <SlickSlider {...settings}>{children}</SlickSlider>;
-
-export default Slider;
