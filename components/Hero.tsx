6ac98d4c29cb69f3bb51e16bb9de1f19b11c4d62
Konstantin M <humbl3dev@gmail.com>
2020-04-21 23:05:12 -0700

    homepage design updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,80 +1,42 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 const ANIMATION_SPEED = 0.3
 const Y_OFFSET = -45
 
-export default () => (
-  <>
-    <div className="hero mb-0">
+const Hero = () => {
+  return (
+    <div className="hero">
       <div className="hero__inner">
-        <div className="hero__intro">
-          <motion.h1
-            className="hero__heading"
-            transition={{
-              duration: ANIMATION_SPEED
-            }}
-            initial={{
-              opacity: 0.01,
-              y: Y_OFFSET
-            }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-          >
-            Let's build you a website.
-          </motion.h1>
-          <motion.p
-            className="hero__subheading"
-            transition={{
-              duration: ANIMATION_SPEED,
-              delay: ANIMATION_SPEED
-            }}
-            initial={{
-              opacity: 0.01,
-              y: Y_OFFSET
-            }}
-            animate={{
-              opacity: 1,
-              y: 0
-            }}
-          >
-            Hi there. We are Apkomatic - a company specializing in building high
-            quality web solutions.
-          </motion.p>
+        <motion.img
+          initial={{
+            y: -25,
+            opacity: 0.25
+          }}
+          transition={{
+            duration: 0.77
+          }}
+          animate={{
+            y: 0,
+            opacity: 1
+          }}
+          className="hero__image"
+          src="/static/images/home/hero-image.svg"
+          alt="Apkomatic Hero"
+        />
+        <h1 className="hero__heading">Let's Build You a Website.</h1>
+        <p className="hero__subheading">
+          We are Apkomatic - a company specializing in building high quality web
+          solutions.
+        </p>
+        <div>
           <Link href="/contact">
-            <motion.a
-              className="hero__cta btn btn-lg btn-secondary"
-              transition={{
-                duration: ANIMATION_SPEED,
-                delay: ANIMATION_SPEED
-              }}
-              initial={{
-                opacity: 0.01,
-                y: 45
-              }}
-              animate={{
-                opacity: 1,
-                y: 0
-              }}
-            >
-              Let's Start Building
-            </motion.a>
+            <a className="hero__cta-btn btn btn-primary btn-lg">Contact Us</a>
           </Link>
-        </div>
-        <div className="hero__image">
-          <img
-            src="/static/hero-graphic-v2.svg"
-            alt="hero image"
-            className="img-fluid animated fadeInUp"
-            style={{
-              animationDuration: '400ms',
-              animationDelay: '1000ms'
-            }}
-          />
         </div>
       </div>
     </div>
-  </>
-)
+  )
+}
+
+export default Hero
