f4e00045aacbd6b6331a01995bfbf6718323ac82
Konstantin M <humbl3dev@gmail.com>
2020-04-21 23:04:18 -0700

    makes contact us a regular link in header

--- config/global.ts
+++ config/global.ts
@@ -1,77 +1,76 @@
-export const IS_DEV = process.env.NODE_ENV !== "production"
+export const IS_DEV = process.env.NODE_ENV !== 'production'
 export const GUA_TRACKING_ID =
-  process.env.NODE_ENV === "production" ? "UA-101945546-1" : "UA-119740251-1"
+  process.env.NODE_ENV === 'production' ? 'UA-101945546-1' : 'UA-119740251-1'
 export const SENTRY_KEY =
-  process.env.NODE_ENV === "production"
-    ? "https://7dcedfd697fa495cb02c6bfd0e177260@sentry.io/1284502"
-    : "https://b437d768b74a4b179edcb38b4d801a60@sentry.io/1284503"
+  process.env.NODE_ENV === 'production'
+    ? 'https://7dcedfd697fa495cb02c6bfd0e177260@sentry.io/1284502'
+    : 'https://b437d768b74a4b179edcb38b4d801a60@sentry.io/1284503'
 export const DISPLAY_WORK = false
 export const CONTACT_ENDPOINT =
-  process.env.NODE_ENV === "production"
-    ? "https://formspree.io/apkomatic@gmail.com"
-    : "https://formspree.io/konstantindev925@gmail.com"
+  process.env.NODE_ENV === 'production'
+    ? 'https://formspree.io/apkomatic@gmail.com'
+    : 'https://formspree.io/konstantindev925@gmail.com'
 export const EMAIL_CONFIRMATION_URL =
-  process.env.NODE_ENV === "production"
-    ? "//apkomatic.com/thank-you"
-    : "//localhost:3000/thank-you"
+  process.env.NODE_ENV === 'production'
+    ? '//apkomatic.com/thank-you'
+    : '//localhost:3000/thank-you'
 export const LINKS = [
   {
     id: 1,
-    href: "/",
-    pathname: "/",
-    label: "Home",
+    href: '/',
+    pathname: '/',
+    label: 'Home',
     active: true
   },
   {
     id: 2,
-    href: "/services",
-    pathname: "/services",
-    label: "Services",
+    href: '/services',
+    pathname: '/services',
+    label: 'Services',
     active: true
   },
   {
     id: 3,
-    href: "/work",
-    pathname: "/work",
-    label: "Work",
+    href: '/work',
+    pathname: '/work',
+    label: 'Work',
     active: DISPLAY_WORK
   },
   {
     id: 4,
-    href: "/about",
-    pathname: "/about",
-    label: "About Us",
+    href: '/about',
+    pathname: '/about',
+    label: 'About Us',
     active: true
   },
   {
     id: 5,
-    href: "/faq",
-    pathname: "/faq",
-    label: "FAQ",
+    href: '/faq',
+    pathname: '/faq',
+    label: 'FAQ',
     active: true
   },
   {
     id: 6,
-    href: "/contact",
-    pathname: "/contact",
-    label: "Contact Us",
-    active: true,
-    isButton: true
+    href: '/contact',
+    pathname: '/contact',
+    label: 'Contact Us',
+    active: true
   }
 ]
 export const SOCIAL = [
   {
     id: 1,
-    href: "https://twitter.com/apkomatic",
-    label: "twitter",
-    iconLabel: "twitter",
+    href: 'https://twitter.com/apkomatic',
+    label: 'twitter',
+    iconLabel: 'twitter',
     active: true
   },
   {
     id: 2,
-    href: "https://www.instagram.com/apkomatic",
-    label: "instagram",
-    iconLabel: "instagram",
+    href: 'https://www.instagram.com/apkomatic',
+    label: 'instagram',
+    iconLabel: 'instagram',
     active: false
   }
 ]
