76bf46c36367a3744de04ece824e95fb880209e1
Konstantin M <humbl3dev@gmail.com>
2020-04-28 14:00:31 -0700

    reimplement service cards using css grid

--- components/servicePage/ServiceOptions.tsx
+++ components/servicePage/ServiceOptions.tsx
@@ -1,124 +1,112 @@
-import React, { memo } from "react"
-import Link from "next/link"
-import { Checkmark } from ".."
+import React, { memo } from 'react'
+import Link from 'next/link'
+import { Checkmark } from '..'
 
-import "../Card.scss"
+import './ServiceOptions.scss'
 
 const ServiceOptions = memo(() => (
-  <div className="services-card-stack row d-flex align-items-stretch mt-3 pt-3">
+  <div className="services">
     <div
-      className="col-lg-4 mb-3 animated fadeIn"
-      style={{ animationDelay: "200ms" }}
+      className="service-block"
+      style={{
+        borderTop: '8px solid #FFBF69'
+      }}
     >
-      <div
-        className="card h-100 text-left"
-        style={{
-          borderTop: "8px solid #FFBF69"
-        }}
-      >
-        <div className="card-body bg-light">
-          <h2>Essential</h2>
-          <p className="text-muted">
-            Perfect for clean, light, and interactive websites.
-          </p>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <Checkmark /> Lightweight and Clean
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> Basic SEO
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> 1-4 pages
-          </li>
-          <li className="list-group-item">&ndash;</li>
-        </ul>
-        <div className="card-body">
-          <Link href="/contact">
-            <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
-              Contact for price
-            </a>
-          </Link>
-        </div>
+      <div className="service-block__heading">
+        <h2>Essential</h2>
+        <p className="text-muted">
+          Perfect for clean, light, and interactive websites.
+        </p>
       </div>
-    </div>
-
-    <div className="col-lg-4 mb-3 animated fadeIn">
-      <div
-        className="card h-100 text-left"
-        style={{
-          borderTop: "8px solid #DB324D"
-        }}
-      >
-        <div className="card-body bg-light">
-          <h2>Balanced</h2>
-          <p className="text-muted">
-            Perfect for more complicated websites with more content and rich
-            interactions.
-          </p>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <Checkmark /> Clean Design
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> Interactive
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> Basic SEO
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> 5-10 pages
-          </li>
-        </ul>
-        <div className="card-body">
-          <Link href="/contact">
-            <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
-              Contact for price
-            </a>
-          </Link>
-        </div>
+      <ul className="service-block__list">
+        <li>
+          <Checkmark /> Lightweight and Clean
+        </li>
+        <li>
+          <Checkmark /> Basic SEO
+        </li>
+        <li>
+          <Checkmark /> 1-4 pages
+        </li>
+        <li>&ndash;</li>
+      </ul>
+      <div className="service-block__callout">
+        <Link href="/contact">
+          <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
+            Contact for price
+          </a>
+        </Link>
       </div>
     </div>
 
     <div
-      className="col-lg-4 mb-3 animated fadeIn"
-      style={{ animationDelay: "200ms" }}
+      className="service-block"
+      style={{
+        borderTop: '8px solid #DB324D'
+      }}
     >
-      <div
-        className="card h-100 text-left"
-        style={{
-          borderTop: "8px solid #6C9DD1"
-        }}
-      >
-        <div className="card-body bg-light">
-          <h2>Advanced</h2>
-          <p className="text-muted">
-            Perfect for more complicated products, like e-commerce or web apps.
-          </p>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <Checkmark /> Fast and Dynamic
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> API/Database Integration
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> Social Media API
-          </li>
-          <li className="list-group-item">
-            <Checkmark /> Unlimited Pages
-          </li>
-        </ul>
-        <div className="card-body">
-          <Link href="/contact">
-            <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
-              Contact for price
-            </a>
-          </Link>
-        </div>
+      <div className="service-block__heading">
+        <h2>Balanced</h2>
+        <p className="text-muted">
+          Perfect for more complicated websites with more content and rich
+          interactions.
+        </p>
+      </div>
+      <ul className="service-block__list">
+        <li>
+          <Checkmark /> Clean Design
+        </li>
+        <li>
+          <Checkmark /> Interactive
+        </li>
+        <li>
+          <Checkmark /> Basic SEO
+        </li>
+        <li>
+          <Checkmark /> 5-10 pages
+        </li>
+      </ul>
+      <div className="service-block__callout">
+        <Link href="/contact">
+          <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
+            Contact for price
+          </a>
+        </Link>
+      </div>
+    </div>
+
+    <div
+      className="service-block"
+      style={{
+        borderTop: '8px solid #6C9DD1'
+      }}
+    >
+      <div className="service-block__heading">
+        <h2>Advanced</h2>
+        <p className="text-muted">
+          Perfect for more complicated products, like e-commerce or web apps.
+        </p>
+      </div>
+      <ul className="service-block__list">
+        <li>
+          <Checkmark /> Fast and Dynamic
+        </li>
+        <li>
+          <Checkmark /> API/Database Integration
+        </li>
+        <li>
+          <Checkmark /> Social Media API
+        </li>
+        <li>
+          <Checkmark /> Unlimited Pages
+        </li>
+      </ul>
+      <div className="service-block__callout">
+        <Link href="/contact">
+          <a className="btn btn-tertiary d-block" tabIndex={0} role="button">
+            Contact for price
+          </a>
+        </Link>
       </div>
     </div>
   </div>
