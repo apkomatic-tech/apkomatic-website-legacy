eac01888c9917e51aebeed8841754879cfe84537
Konstantin M <humbl3dev@gmail.com>
2020-04-27 20:39:10 -0700

    updates splash component

--- components/Splash.tsx
+++ components/Splash.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import "./Splash.scss"
+import React from 'react'
+import './Splash.scss'
 
 interface ISplash extends JSX.IntrinsicAttributes {
   title: string
@@ -7,22 +7,13 @@ interface ISplash extends JSX.IntrinsicAttributes {
   splashStyle?: string
 }
 
-const Splash = ({ title, text, splashStyle = "splash--alt1" }: ISplash) => (
+const Splash = ({ title, text, splashStyle = 'splash--alt1' }: ISplash) => (
   <div className={`splash ${splashStyle}`}>
     <div className="container splash__inner">
       <div className="text-center">
-        <h1
-          className="splash__title animated fadeInDown text-center"
-          style={{ animationDuration: "500ms", animationDelay: "200ms" }}
-        >
-          {title}
-        </h1>
+        <h1 className="splash__title text-center">{title}</h1>
         {text && (
-          <p
-            className="lead animated fadeInUp"
-            style={{ animationDelay: "700ms" }}
-            data-testid="splash-text"
-          >
+          <p className="lead" data-testid="splash-text">
             {text}
           </p>
         )}
