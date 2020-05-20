726c9c4da93c414d486fab8ed6b994b1af9396bc
Konstantin M <humbl3dev@gmail.com>
2020-05-20 15:30:53 -0700

    adds name validation

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
-import { validateEmail } from '../utils'
+import { validateEmail, validateName } from '../utils'
 
 test('validateEmail', () => {
   expect(validateEmail('me@example.com')).toBe(true)
   expect(validateEmail('me@example2.com')).toBe(true)
   expect(validateEmail('me@')).toBe(false)
   expect(validateEmail('')).toBe(false)
+})
+
+test('validateName', () => {
+  expect(validateName('hello')).toBe(true)
+  expect(validateName('john doe')).toBe(true)
+  expect(validateName('')).toBe(false)
+  expect(validateName('john doe1')).toBe(false)
 })
